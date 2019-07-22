71519a619f580cfaa98cabc22347a749b5f5fa0a
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2019-07-22 13:32:11 +0300

    Export stack outputs (#89)

--- __tests__/outputs.test.js
+++ __tests__/outputs.test.js
@@ -1,3 +1,4 @@
+const { VALID_SUBNET_GROUPS } = require('../src/constants');
 const { buildOutputs } = require('../src/outputs');
 
 describe('outputs', () => {
@@ -84,6 +85,74 @@ describe('outputs', () => {
       };
 
       const actual = buildOutputs(true);
+      expect(actual).toEqual(expected);
+      expect.assertions(1);
+    });
+
+    it('exports the stack output', () => {
+      const expected = {
+        DAXSubnetGroup: {
+          Description: 'Subnet Group for dax',
+          Export: {
+            Name: {
+              'Fn::Join': ['-', ["Fn::Ref 'AWS::StackName'", 'DAXSubnetGroup']],
+            },
+          },
+          Value: ['DAXSubnetGroup'],
+        },
+        ElastiCacheSubnetGroup: {
+          Description: 'Subnet Group for redshift',
+          Export: {
+            Name: {
+              'Fn::Join': ['-', ["Fn::Ref 'AWS::StackName'", 'ElastiCacheSubnetGroup']],
+            },
+          },
+          Value: ['ElastiCacheSubnetGroup'],
+        },
+        LambdaExecutionSecurityGroupId: {
+          Description:
+            'Security Group logical resource ID that the Lambda functions use when executing within the VPC',
+          Export: {
+            Name: {
+              'Fn::Join': ['-', ["Fn::Ref 'AWS::StackName'", 'LambdaExecutionSecurityGroupId']],
+            },
+          },
+          Value: {
+            Ref: 'LambdaExecutionSecurityGroup',
+          },
+        },
+        RDSSubnetGroup: {
+          Description: 'Subnet Group for rds',
+          Export: {
+            Name: {
+              'Fn::Join': ['-', ["Fn::Ref 'AWS::StackName'", 'RDSSubnetGroup']],
+            },
+          },
+          Value: ['RDSSubnetGroup'],
+        },
+        RedshiftSubnetGroup: {
+          Description: 'Subnet Group for elasticache',
+          Export: {
+            Name: {
+              'Fn::Join': ['-', ["Fn::Ref 'AWS::StackName'", 'RedshiftSubnetGroup']],
+            },
+          },
+          Value: ['RedshiftSubnetGroup'],
+        },
+        VPC: {
+          Description: 'VPC logical resource ID',
+          Export: {
+            Name: {
+              'Fn::Join': ['-', ["Fn::Ref 'AWS::StackName'", 'VPC']],
+            },
+          },
+          Value: {
+            Ref: 'VPC',
+          },
+        },
+      };
+
+      const actual = buildOutputs(false, VALID_SUBNET_GROUPS, true);
       expect(actual).toEqual(expected);
       expect.assertions(1);
     });
--- src/outputs.js
+++ src/outputs.js
@@ -1,42 +1,6 @@
 const { VALID_SUBNET_GROUPS } = require('./constants');
 
-/**
- * Build CloudFormation Outputs on common resources
- *
- * @param {Boolean} createBastionHost
- * @return {Object}
- */
-
-function buildOutputs(createBastionHost = false, subnetGroups = VALID_SUBNET_GROUPS) {
-  const outputs = {
-    VPC: {
-      Description: 'VPC logical resource ID',
-      Value: {
-        Ref: 'VPC',
-      },
-    },
-    LambdaExecutionSecurityGroupId: {
-      Description:
-        'Security Group logical resource ID that the Lambda functions use when executing within the VPC',
-      Value: {
-        Ref: 'LambdaExecutionSecurityGroup',
-      },
-    },
-  };
-
-  if (createBastionHost) {
-    outputs.BastionSSHUser = {
-      Description: 'SSH username for the Bastion host',
-      Value: 'ec2-user',
-    };
-    outputs.BastionEIP = {
-      Description: 'Public IP of Bastion host',
-      Value: {
-        Ref: 'BastionEIP',
-      },
-    };
-  }
-
+function appendSubnets(subnetGroups, outputs) {
   if (subnetGroups) {
     const typesToNames = {
       rds: 'RDSSubnetGroup',
@@ -54,6 +18,67 @@ function buildOutputs(createBastionHost = false, subnetGroups = VALID_SUBNET_GRO
 
     Object.assign(outputs, ...subnetOutputs);
   }
+}
+
+function appendBastionHost(createBastionHost, outputs) {
+  if (createBastionHost) {
+    outputs.BastionSSHUser = {
+      Description: 'SSH username for the Bastion host',
+      Value: 'ec2-user',
+    };
+    outputs.BastionEIP = {
+      Description: 'Public IP of Bastion host',
+      Value: {
+        Ref: 'BastionEIP',
+      },
+    };
+  }
+}
+
+function appendExports(exportOutputs, outputs) {
+  if (exportOutputs) {
+    Object.entries(outputs).forEach(([name, value]) => {
+      // eslint-disable-next-line no-param-reassign
+      value.Export = { Name: { 'Fn::Join': ['-', ["Fn::Ref 'AWS::StackName'", name]] } };
+    });
+  }
+}
+
+/**
+ * Build CloudFormation Outputs on common resources
+ *
+ * @param {Boolean} createBastionHost
+ * @param {String[]} subnetGroups
+ * @param {Boolean} exportOutputs
+ * @return {Object}
+ */
+
+function buildOutputs(
+  createBastionHost = false,
+  subnetGroups = VALID_SUBNET_GROUPS,
+  exportOutputs = false,
+) {
+  const outputs = {
+    VPC: {
+      Description: 'VPC logical resource ID',
+      Value: {
+        Ref: 'VPC',
+      },
+    },
+    LambdaExecutionSecurityGroupId: {
+      Description:
+        'Security Group logical resource ID that the Lambda functions use when executing within the VPC',
+      Value: {
+        Ref: 'LambdaExecutionSecurityGroup',
+      },
+    },
+  };
+
+  appendSubnets(subnetGroups, outputs);
+
+  appendBastionHost(createBastionHost, outputs);
+
+  appendExports(exportOutputs, outputs);
 
   return outputs;
 }
